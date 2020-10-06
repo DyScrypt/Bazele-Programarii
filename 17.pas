eef2eeb5b8fe4dcb7401550d75eca1b2463f3223
DyScrypt <71259649+DyScrypt@users.noreply.github.com>
2020-10-06 16:02:51 +0300

    Add files via upload

---
+++ 17.pas
@@ -0,0 +1,15 @@
+﻿var x,y,z,a,s,d,q,w:integer;
+begin
+ readln(x,y);
+ a:=385 div 60;
+ s:=385 mod 60;
+ if s+y > 60 then begin
+   w:=(s+y)-60;
+   x:=x+1;
+ end;
+
+    
+ q:=x+a;
+ 
+ writeln(q,' ',w);
+end.
