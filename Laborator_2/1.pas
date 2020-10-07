9f389ec1795866632bf8e0e20ee6da13912084ab
Johny <iongirbu12@mail.ru>
2020-10-07 12:13:07 +0300

    Update

---
+++ Laborator_2/1.pas
@@ -0,0 +1,18 @@
+﻿var a,b,c,d,e,f,r,t:integer;
+begin
+  readln(a,b,c);
+  if (a>b) and (a>c) then
+    e:=a else
+      if (b>a) and (b>c) then
+        e:=b
+        else
+        e:=c;
+    if (a<b) and (a<c) then
+    f:=a else
+      if (b<a) and (b<c) then
+        f:=b
+        else
+        f:=c;
+        r:=e-f;
+    writeln(r);
+end.
